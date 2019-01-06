2333ea1b46dc46285a02e2659278614f9a338393
Inka Luostarinen <inka.luostarinen@eng.tamk.fi>
2019-01-06 13:16:21 +0200

    edit done

--- src/components/Loans.tsx
+++ src/components/Loans.tsx
@@ -18,6 +18,25 @@ class Loans extends React.Component<{}, ILoansState> {
     this.state ={
       data: loans
     }
+    this.renderEditable = this.renderEditable.bind(this);
+
+    }
+    renderEditable(cellInfo) {
+      return (
+        <div
+       style={{ backgroundColor: "#fafafa" }}
+          contentEditable
+          suppressContentEditableWarning
+          onBlur={e => {
+            const data = [...this.state.data];
+            data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+            this.setState({ data });
+          }}
+          dangerouslySetInnerHTML={{
+            __html: this.state.data[cellInfo.index][cellInfo.column.id]
+          }}
+        />
+      );
     }
 
   public render() {
@@ -38,40 +57,45 @@ class Loans extends React.Component<{}, ILoansState> {
                     id: "equipmentId",
                     accessor: d => d.equipmentId,
                     filterMethod: (filter, rows) =>
-                            matchSorter(rows, filter.value, { keys: ["equipmentId"] }),
-                          filterAll: true
+                    matchSorter(rows, filter.value, { keys: ["equipmentId"] }),
+                    filterAll: true,
+                    Cell: this.renderEditable
                   },
                   {
                     Header: " User ",
                     id: "id",
                     accessor: d => d.id,
                     filterMethod: (filter, rows) =>
-                            matchSorter(rows, filter.value, { keys: ["id"] }),
-                          filterAll: true
+                    matchSorter(rows, filter.value, { keys: ["id"] }),
+                    filterAll: true,
+                    Cell: this.renderEditable
                   },
                   {
                     Header: "Begins",
                     id: "begins",
                     accessor: d => d.begins,
                     filterMethod: (filter, rows) =>
-                            matchSorter(rows, filter.value, { keys: ["begins"] }),
-                          filterAll: true
+                    matchSorter(rows, filter.value, { keys: ["begins"] }),
+                    filterAll: true,
+                    Cell: this.renderEditable
                   },
                   {
                     Header: "Ends",
                     id: "ends",
                     accessor: d => d.ends,
                     filterMethod: (filter, rows) =>
-                            matchSorter(rows, filter.value, { keys: ["ends"] }),
-                          filterAll: true
+                    matchSorter(rows, filter.value, { keys: ["ends"] }),
+                    filterAll: true,
+                    Cell: this.renderEditable
                   },
                   {
                     Header: "Returned",
                     id: "returned",
                     accessor: d => d.returned,
                     filterMethod: (filter, rows) =>
-                            matchSorter(rows, filter.value, { keys: ["returned"] }),
-                          filterAll: true
+                    matchSorter(rows, filter.value, { keys: ["returned"] }),
+                    filterAll: true,
+                    Cell: this.renderEditable
                   }
                 ]
               }
--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -18,6 +18,24 @@ class Users extends React.Component<{}, IUsersState> {
     this.state ={
       data: users
     }
+    this.renderEditable=this.renderEditable.bind(this);
+    }
+    renderEditable(cellInfo) {
+      return (
+        <div
+       style={{ backgroundColor: "#fafafa" }}
+          contentEditable
+          suppressContentEditableWarning
+          onBlur={e => {
+            const data = [...this.state.data];
+            data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+            this.setState({ data });
+          }}
+          dangerouslySetInnerHTML={{
+            __html: this.state.data[cellInfo.index][cellInfo.column.id]
+          }}
+        />
+      );
     }
 
   public render() {
@@ -40,8 +58,9 @@ class Users extends React.Component<{}, IUsersState> {
             id: "id",
             accessor: d => d.id,
             filterMethod: (filter, rows) =>
-                    matchSorter(rows, filter.value, { keys: ["id"] }),
-                  filterAll: true
+            matchSorter(rows, filter.value, { keys: ["id"] }),
+            filterAll: true,
+            Cell: this.renderEditable
 
           },
           {
@@ -49,16 +68,20 @@ class Users extends React.Component<{}, IUsersState> {
             id: "name",
             accessor: d => d.name,
             filterMethod: (filter, rows) =>
-                    matchSorter(rows, filter.value, { keys: ["name"] }),
-                  filterAll: true
+            matchSorter(rows, filter.value, { keys: ["name"] }),
+            filterAll: true,
+            Cell: this.renderEditable
+
           },
           {
             Header: "user email",
             id: "email",
             accessor: d => d.email,
             filterMethod: (filter, rows) =>
-                    matchSorter(rows, filter.value, { keys: ["email"] }),
-                  filterAll: true
+            matchSorter(rows, filter.value, { keys: ["email"] }),
+            filterAll: true,
+            Cell: this.renderEditable
+
           }
         ]
       }
